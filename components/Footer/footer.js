680fff0db29f433f0262956810747c4ff6e87b1e
dialkq <dialkq@gmail.com>
2021-01-26 12:06:17 +0700

    create footer components

---
+++ components/Footer/footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return(
+    <>
+      <footer className="text-center md:text-left lg:text-left xl:text-left italic font-mono text-lg lg:text-xl xl:text-xl font-bold">&copy;
+        2020 Dwi Abdul kholiq
+      </footer>
+    </>
+  )
+}
+
+export default Footer
